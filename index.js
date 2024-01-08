4e9a8bf960d36d727e1b4e51b0da9692ce7310eb
Al-bashir <albashirabdi482@gmail.com>
2024-01-08 14:30:02 +0300

    Comleted Assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(x,y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+function divide(x,y){
+    return x/y;
+}
+function multiply(x,y){
+    return x*y;
+}
+
+function increment(x){
+    return ++x;
+}
+function decrement(x){
+    return --x;
+}
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
